be52954a1807c800e95b31592bbeb48b20ff6785
Ubuntu <howardc93@gmail.com>
2016-09-05 07:34:20 +0000

    fix tag

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$DOCKER_USERNAME" ]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker tag yasp/parser:latest yasp/yasp:${TRAVIS_COMMIT}
+  docker tag yasp/parser:latest yasp/parser:${TRAVIS_COMMIT}
   docker push yasp/parser:${TRAVIS_COMMIT}
   docker push yasp/parser:latest
 fi
