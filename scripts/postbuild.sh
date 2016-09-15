2dab48f0f0e6656bc61de0c88d9666693ec01ae7
Howard Chung <howardc93@gmail.com>
2016-09-14 19:48:51 -0700

    Update postbuild.sh

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$DOCKER_USERNAME" ]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker tag yasp/parser:latest yasp/parser:${TRAVIS_COMMIT}
-  docker push yasp/parser:${TRAVIS_COMMIT}
-  docker push yasp/parser:latest
+  docker tag odota/parser:latest odota/parser:${TRAVIS_COMMIT}
+  docker push odota/parser:${TRAVIS_COMMIT}
+  docker push odota/parser:latest
 fi
