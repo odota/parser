063e10cd109cde8207c0f3c303097bcbd5227145
Ubuntu <howardc93@gmail.com>
2016-09-20 06:56:34 +0000

    update rebuild script

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo docker build -t yasp/parser .
+sudo docker build -t odota/parser .
 sudo docker rm -fv parser
-sudo docker run -d --name parser --net=host yasp/parser
+sudo docker run -d --name parser --net=host odota/parser
