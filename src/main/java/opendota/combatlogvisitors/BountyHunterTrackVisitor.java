d4f4049c677ffbc776050ff5d3140f5858badb80
Evaldo Bratti <evaldo.bratti@gmail.com>
2017-04-03 22:52:27 -0300

    joining &&

--- src/main/java/opendota/combatlogvisitors/BountyHunterTrackVisitor.java
+++ src/main/java/opendota/combatlogvisitors/BountyHunterTrackVisitor.java
@@ -11,25 +11,23 @@ import opendota.Parse.Entry;
 public class BountyHunterTrackVisitor implements Visitor<Boolean> {
 
 	private static final String BH_TRACK = "modifier_bounty_hunter_track";
-	
+
 	HashMap<String, Boolean> trackStatus = new HashMap<String, Boolean>();
 
 	@Override
 	public Boolean visit(Entry entry) {
-        if (entry.type.equals(DOTA_COMBATLOG_MODIFIER_ADD) && entry.inflictor.equals(BH_TRACK)) {
+		if (entry.type.equals(DOTA_COMBATLOG_MODIFIER_ADD) && entry.inflictor.equals(BH_TRACK)) {
 			trackStatus.put(entry.targetname, true);
-        }
-        
-        if (entry.type.equals(DOTA_COMBATLOG_MODIFIER_REMOVE) && entry.inflictor.equals(BH_TRACK)) {
+		}
+
+		if (entry.type.equals(DOTA_COMBATLOG_MODIFIER_REMOVE) && entry.inflictor.equals(BH_TRACK)) {
 			trackStatus.put(entry.targetname, false);
-        }
-        
-        if (entry.type.equals(DOTA_COMBATLOG_DEATH)) {
-			if (trackStatus.getOrDefault(entry.targetname, false)) {
-				return true; 
-			}
-        }
-        
-        return null;
+		}
+
+		if (entry.type.equals(DOTA_COMBATLOG_DEATH) && trackStatus.getOrDefault(entry.targetname, false)) {
+			return true;
+		}
+
+		return null;
 	}
 }
