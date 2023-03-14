13c3a5077165402b77e69b740eb0fbcb1fdc2677
Howard Chung <howardzchung@gmail.com>
2023-03-14 11:26:00 -0700

    Update postbuild.sh

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -4,6 +4,6 @@ if [ -n "$DOCKER_USERNAME" ]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker tag odota/parser:latest
   docker push odota/parser:latest
-  docker tag odota/parser:latest howardc93/parser:latest
+  docker tag odota/parser howardc93/parser:latest
   docker push howard93/parser:latest
 fi
