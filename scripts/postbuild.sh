57bbc80d824a5cae877e8ddf0c9250fb7ab008e3
Howard Chung <howardzchung@gmail.com>
2025-11-21 17:54:32 -0800

    Delete scripts/postbuild.sh

--- scripts/postbuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -n "$DOCKER_USERNAME" ]; then
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker tag odota/parser:latest
-  docker push odota/parser:latest
-  docker tag odota/parser howardc93/parser:latest
-  docker push howardc93/parser:latest
-fi
