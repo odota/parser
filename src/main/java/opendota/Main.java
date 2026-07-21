60792228b966394ea166d3e5ee6b2db3ce461b6a
Howard Chung <howardzchung@gmail.com>
2026-07-21 11:46:05 -0700

    Update Main.java

--- src/main/java/opendota/Main.java
+++ src/main/java/opendota/Main.java
@@ -227,7 +227,7 @@ class RegisterTask extends TimerTask {
                     // Otherwise, use hostname -i to get internal IP
                     ip = RegisterTask.shellExec("hostname -i");
                 }
-                long nproc = Math.min(Runtime.getRuntime().availableProcessors() * 2, 16);
+                long nproc = Math.min(Runtime.getRuntime().availableProcessors() * 2, 12);
                 String postCmd = "curl -X POST --max-time 60 -L " + System.getenv().get("SERVICE_REGISTRY_HOST")
                         + "/register/parser/" + ip + ":5600" + "?size=" + nproc + "&key="
                         + System.getenv().get("RETRIEVER_SECRET");
